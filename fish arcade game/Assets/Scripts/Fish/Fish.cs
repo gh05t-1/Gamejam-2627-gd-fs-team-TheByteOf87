d42ee8d9baf6b75cc2603adbf1383d67b1969313
Liv_Alles <38350@ma-web.nl>
2026-09-02 11:31:11 +0200

    fish

---
+++ fish arcade game/Assets/Scripts/Fish/Fish.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Fish")]
+public class Fish : ScriptableObject
+{
+    public int lenght;
+    public int points;
+    public string fishName;
+    public Sprite icon;
+}
