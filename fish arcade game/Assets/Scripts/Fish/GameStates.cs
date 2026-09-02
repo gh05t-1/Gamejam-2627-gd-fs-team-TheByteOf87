d2baec0580fccf7d215a510408f3688250f03e43
Skyler <39634@ma-web.nl>
2026-09-02 12:47:58 +0200

    Updatinjg

---
+++ fish arcade game/Assets/Scripts/Fish/GameStates.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameStates : MonoBehaviour
+{
+
+
+   /* private GameStates currentState;
+    private J
+
+    public enum GameStates
+    {
+        Fishing,
+        Boating
+    }
+
+    private void Start()
+    {
+        FishingMiniGame = GetComponent<>();
+    }
+    void update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)){
+            if (currentState == GameStates.Fishing)
+            {
+                currentState = GameStates.Boating;
+            }
+        }
+        else
+        {
+            currentState = GameStates.Fishing;
+        }
+    }*/
+}
