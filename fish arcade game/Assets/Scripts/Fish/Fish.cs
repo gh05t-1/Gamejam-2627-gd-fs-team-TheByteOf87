e18cfd218b6f71714a83342d3b080a522eb6d11e
Liv_Alles <38350@ma-web.nl>
2026-09-02 13:10:56 +0200

    Fish randomizer done
    made it so you can randomize the fish and add scripted objects to make more fish

--- fish arcade game/Assets/Scripts/Fish/Fish.cs
+++ fish arcade game/Assets/Scripts/Fish/Fish.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Fish")]
 public class Fish : ScriptableObject
 {
-    public int lenght;
+    public int fishLenght;
     public int points;
     public string fishName;
     public Sprite icon;
--- fish arcade game/Assets/Scripts/Fish/FishRandomizer.cs
+++ fish arcade game/Assets/Scripts/Fish/FishRandomizer.cs
@@ -1,7 +1,34 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class FishRandomizer : MonoBehaviour
 {
-    public Fish fish;
+    //private Fish fish;
+    public List<Fish> fishList = new List<Fish>();
+    private Fish chosenFish;
+
+
+    [SerializeField] private Image FishIcon;
+    [SerializeField] private TextMeshProUGUI fishStats;
+
+    private void Start()
+    {
+        FishIcon.enabled = false;
+    }
+    private void Update()
+    {
+ 
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            chosenFish = fishList[Random.Range(0, fishList.Count)];
+            FishIcon.enabled = true;
+            FishIcon.sprite = chosenFish.icon;
+            fishStats.text = "Fish; " + chosenFish.fishName + " Length: " + chosenFish.fishLenght + " Points " + chosenFish.points;
+        }
+        
+        
+    }
 
 }
